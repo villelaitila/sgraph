eb4db6dc158fc201023fd7446edbeced490ae21d
Ville Laitila <ville.laitila@softagram.com>
2026-02-05 15:24:31 +0200

    support for __version__

--- src/sgraph/__init__.py
+++ src/sgraph/__init__.py
@@ -1,6 +1,13 @@
+from importlib.metadata import version, PackageNotFoundError
+
 from sgraph.sgraph import SGraph
 from sgraph.selement import SElement
 from sgraph.exceptions import SElementMergedException, ModelNotFoundException
 from sgraph.selementassociation import SElementAssociation
 from sgraph.modelapi import ModelApi
 from sgraph.metricsapi import MetricsApi
+
+try:
+    __version__ = version("sgraph")
+except PackageNotFoundError:
+    __version__ = "unknown"
